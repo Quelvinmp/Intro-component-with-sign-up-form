69fd367fb3f5ec6d33087c09574b9a96ccd0dd9f
Quelvinmp <quelvinmp@gmail.com>
2023-01-12 12:59:35 -0300

    finish styling all html

---
+++ script.js
@@ -0,0 +1,25 @@
+const submitButton = document.getElementById('btn-submit');
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const email = document.getElementById('e-mail');
+const password = document.getElementById('password');
+
+
+const validateFields = () => {
+
+};
+
+const validateEmail = () => {
+
+};
+
+const submitForm = () => {
+    submitButton.addEventListener('click', () => {
+
+    });
+}
+
+window.onload = () => {
+    submitForm();
+
+}
